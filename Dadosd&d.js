3763696ae7f4e68d20f04aa8de03c9e4355b27e2
lirathiagoh <106279019+lirathiagoh@users.noreply.github.com>
2022-05-25 17:47:55 -0300

    Add files via upload

---
+++ Dadosd&d.js
@@ -0,0 +1,30 @@
+function dado20(){
+    document.getElementById('ResultadoD20').innerHTML = Math.floor(Math.random() * 20 + 1);
+
+   
+}
+function dado12(){
+    document.getElementById('ResultadoD12').innerHTML = Math.floor(Math.random() * 12 + 1);
+    
+   
+}
+function dado10(){
+    document.getElementById('ResultadoD10').innerHTML = Math.floor(Math.random() * 10 + 1);
+    
+   
+}
+function dado8(){
+    document.getElementById('ResultadoD8').innerHTML = Math.floor(Math.random() * 8 + 1);
+    
+   
+}
+function dado6(){
+    document.getElementById('ResultadoD6').innerHTML = Math.floor(Math.random() * 6 + 1);
+    
+   
+}
+function dado4(){
+    document.getElementById('ResultadoD4').innerHTML = Math.floor(Math.random() * 4 + 1);
+    
+   
+}
